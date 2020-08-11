fd44a1367516a28596a34a8668b507174b6c6a40
Maxim Eroshenko <eroshenko.maksim.87@mail.ru>
2020-08-11 21:35:16 +0300

    Исправление аргументов.

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,11 +1,10 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int price = 10_000;
         int oneMile = 20;
-        int bonus = price / oneMile;
-        System.out.println(bonus);
+        int miles = cost / oneMile;
+        System.out.println(miles);
 
-        return cost;
+        return miles;
 
     }
 }
